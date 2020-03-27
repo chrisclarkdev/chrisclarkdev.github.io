665edc538df1f3cc10746c89c071920c3c20df92
Chrisclrk9 <chrisclrk9@gmail.com>
2020-03-27 11:57:37 +0000

    Covid 19 tracker
    Covid 19 tracker

---
+++ app.js
@@ -0,0 +1,33 @@
+// https://pomber.github.io/covid19/timeseries.json
+countryName = document.getElementById('c-id');
+active = document.getElementById('active');
+perDeath = document.getElementById('perDeath');
+{/* <img src="https://www.countryflags.io/belgium/flat/64.png"></img> */}
+
+
+function pushData() {
+let country = document.getElementById("country").value;
+
+fetch(`https://coronavirus-19-api.herokuapp.com/countries/${country}`)
+.then(response => {
+  return response.json();
+})
+.catch(err => alert(err))
+.then(countries => {
+  console.log(countries);
+  // console.log(perDeath.toFixed(2));
+  let percentage = (countries.deaths * 100) / countries.cases;
+  if (countries.todayCases < 1 ){
+      casesToday.innerHTML = "Today's Cases: " + 0;
+  } else {
+    casesToday.innerHTML = "Today's Cases: " + countries.todayCases;
+  }
+  countryName.innerHTML = countries.country;
+  active.innerHTML = "Active cases: " + countries.active;
+  cases.innerHTML = "All Cases: " + countries.cases;
+  deaths.innerHTML = "Deaths: " + countries.deaths;
+  recovered.innerHTML = "Recovered: "  + countries.recovered;
+  perDeath.innerHTML = " Percent of deaths: " + percentage.toFixed(2) + '%';
+});
+
+}
